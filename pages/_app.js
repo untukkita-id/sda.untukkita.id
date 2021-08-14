9992223f5623fe5719d2bb380c21992bbb114f4f
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-14 20:54:46 +0700

    chore: add fontawesome phone icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import 'tailwindcss/tailwind.css';
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 config.autoA11y = true;
 
-library.add(faHome);
+library.add(faHome, faPhone);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
