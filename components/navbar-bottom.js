6e19e40eeb0e0e9a56f20c8862c3a68c8ad5f075
Wahyu Dwi Pamungkas <54252239+wahyudwipamungkas@users.noreply.github.com>
2021-08-14 20:59:16 +0700

    chore: change navbar button hover style (#3)
    * Hover-button
    
    * Update-hover-button
    
    * Update Hover-button
    
    Co-authored-by: Wahyu Dwi Pamungkas <54252239+Wahyu-Dwi@users.noreply.github.com>

--- components/navbar-bottom.js
+++ components/navbar-bottom.js
@@ -3,20 +3,20 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavbarButton() {
   return (
-    <div className="fixed bottom-0 bg-gray-600 w-screen h-16 px-8 flex flex-row gap-8 justify-center text-white">
-      <div className="flex flex-col flex-grow items-center justify-center pt-2">
+    <div className="fixed bottom-0 bg-gray-600 w-screen h-16 px-8 flex flex-row gap-4 justify-center text-white">
+      <div className="flex flex-col flex-grow items-center justify-center p-2 active:bg-gray-800 hover:bg-gray-700">
         <FontAwesomeIcon icon={faHome} size="lg" />
         <p>Beranda</p>
       </div>
-      <div className="flex flex-col flex-grow items-center justify-center pt-2">
+      <div className="flex flex-col flex-grow items-center justify-center p-2 active:bg-gray-800 hover:bg-gray-700">
         <FontAwesomeIcon icon={faHome} size="lg" />
         <p>Beranda</p>
       </div>
-      <div className="flex flex-col flex-grow items-center justify-center pt-2">
+      <div className="flex flex-col flex-grow items-center justify-center p-2 active:bg-gray-800 hover:bg-gray-700">
         <FontAwesomeIcon icon={faHome} size="lg" />
         <p>Beranda</p>
       </div>
-      <div className="flex flex-col flex-grow items-center justify-center pt-2">
+      <div className="flex flex-col flex-grow items-center justify-center p-2 active:bg-gray-800 hover:bg-gray-700">
         <FontAwesomeIcon icon={faHome} size="lg" />
         <p>Beranda</p>
       </div>
