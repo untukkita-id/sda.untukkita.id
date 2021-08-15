d77e557fc2525e9085d536aea009ac0b096d23c1
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-15 10:34:58 +0700

    feat: Navigation to data faskes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import 'tailwindcss/tailwind.css';
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faHome, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome,
+  faPhone,
+  faMapMarkerAlt,
+  faClinicMedical,
+} from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 config.autoA11y = true;
 
-library.add(faHome, faPhone, faMapMarkerAlt, faInstagram);
+library.add(faHome, faPhone, faMapMarkerAlt, faInstagram, faClinicMedical);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
