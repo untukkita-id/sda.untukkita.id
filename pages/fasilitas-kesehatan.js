d80212244975503bfa207114bf6327d427caa73e
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-17 17:22:51 +0700

    fix: basic meta description

--- pages/fasilitas-kesehatan.js
+++ pages/fasilitas-kesehatan.js
@@ -10,6 +10,15 @@ import SectionGrub from '../components/sections';
 import DATAFASKES from '../data/faskes-sheets.json';
 
 export default function FasilitasKesehatan() {
+  const siteInfo = {
+    title: 'UntukKita Sidoarjo - Daftar Fasilitas Kesehatan di Kota Sidoarjo',
+    description:
+      'Daftar Fasilitas Kesehatan di kota Sidoarjo. Fasilitas Penunjang Kesembuhan COVID-19 kota Sidoarjo',
+    image: 'opengraph.jpg',
+    url: 'https://covid.sda.untukkita.my.id',
+    pageDescription: 'Kumpulan data fasilitas kesehatan yang terdapat di kota Sidoarjo',
+    pageTitle: 'Data Fasilitas Kesehatan di Kota Sidoarjo',
+  };
   const componentList = [];
 
   for (let i = 0; i < DATAFASKES.length; i += 1) {
@@ -28,13 +37,17 @@ export default function FasilitasKesehatan() {
   return (
     <div id="home" className="text-gray-700">
       <Head>
-        <title>UntukKita Sidoarjo - Daftar Fasilitas Kesehatan di Kota Sidoarjo</title>
+        <title>{siteInfo.title}</title>
+        <meta name="description" content={siteInfo.description} />
+        <meta name="title" content={siteInfo.title} />
+        <meta property="og:url" content="https://covid.sda.untukkita.my.id/" />
+        <meta property="og:title" content={siteInfo.title} />
+        <meta property="og:description" content={siteInfo.description} />
+        <meta property="twitter:title" content={siteInfo.title} />
+        <meta property="twitter:description" content={siteInfo.description} />
       </Head>
       <Header title="Fasilitas Kesehatan" />
-      <PageTitle
-        title="Data Fasilitas Kesehatan di Kota Sidoarjo"
-        description="Kumpulan data fasilitas kesehatan yang terdapat di kota Sidoarjo"
-      />
+      <PageTitle title={siteInfo.pageTitle} description={siteInfo.pageDescription} />
       <SectionGrub title="Data Fasilitas Kesehatan">
         <ul>{componentList}</ul>
       </SectionGrub>
