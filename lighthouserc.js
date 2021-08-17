1967aa45d951475e636a73cc93a5b5ee29731ba0
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-17 17:50:33 +0700

    ci: ignore legacy javascript audits

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      settings: { skipAudits: ['canonical', 'uses-http2', 'uses-long-cache-ttl'] },
+    },
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
         'dom-size': ['error', { maxNumericValue: 3000 }],
-        'legacy-javascript': off,
       },
     },
   },
