bdeb58ebe305861d9fbde9364a7e6169992d84a4
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-17 17:19:10 +0700

    fix: disable legacy javascript assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,9 +4,7 @@ module.exports = {
       preset: 'lighthouse:no-pwa',
       assertions: {
         'dom-size': ['error', { maxNumericValue: 3000 }],
-        'offscreen-images': 'off',
-        'color-contrast': 'off',
-        'tap-targets': 'off',
+        'legacy-javascript': off,
       },
     },
   },
