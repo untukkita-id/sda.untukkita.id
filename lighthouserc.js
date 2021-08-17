7f4c031014cebdadcdbf7338dd4c5b426c850d59
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-17 18:26:44 +0700

    ci: quick assertion on LHCI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,20 @@
 module.exports = {
   ci: {
     collect: {
-      settings: { skipAudits: ['canonical', 'uses-http2', 'uses-long-cache-ttl'] },
+      settings: {
+        skipAudits: [],
+      },
     },
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
         'dom-size': ['error', { maxNumericValue: 3000 }],
+        'first-contentful-paint': ['error', { maxNumericValue: 1000 }],
+        'first-cpu-idle': ['error', { maxNumericValue: 1000 }],
+        'first-meaningful-paint': ['error', { maxNumericValue: 1000 }],
+        'csp-xss': 'off',
+        'legacy-javascript': 'off',
+        'max-potential-fid': ['error', { maxNumericValue: 1500 }],
       },
     },
   },
