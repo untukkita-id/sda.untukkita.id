62f6de8ad7c3292d624334f79e1c49f99f057f50
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-20 23:44:45 +0700

    ci: force disable no-unused asserts check

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,12 +8,23 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
+        // Minimun score for the report to be considered valid
+        'categories:performance': ['error', { minScore: 0.5 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+
+        // Blocking Assertions
         'dom-size': ['error', { maxNumericValue: 3000 }],
         'first-contentful-paint': ['error', { maxNumericValue: 1000 }],
         'first-meaningful-paint': ['error', { maxNumericValue: 1000 }],
+        'max-potential-fid': ['error', { maxNumericValue: 1500 }],
+
+        // Ignored Assertions
+        'categories:seo': ['warn', { minScore: 0.8 }],
+
+        // Disabled Assertions
         'csp-xss': 'off',
         'legacy-javascript': 'off',
-        'max-potential-fid': ['error', { maxNumericValue: 1500 }],
+        'unused-javascript': 'off',
       },
     },
   },
