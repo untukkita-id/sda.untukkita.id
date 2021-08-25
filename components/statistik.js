2c27d182186416ddc3b4a81b8e8ff3e251ae16dc
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-26 01:55:34 +0700

    fix: mock up components for scraper render

--- components/statistik.js
+++ components/statistik.js
@@ -1,24 +1,24 @@
-export default function Statistik({ featuredAngkaKejadian }) {
-  const AngkaKejadian = []
-  for (let i = 0; i < featuredAngkaKejadian.length; i += 1) {
-    AngkaKejadian.push();
+export default function Statistik({ data }) {
+  for (let i = 0; i < data.length - 1; i += 1) {
+    {
+      /* // @@TODO: Add Rendered componentss */
+    }
   }
 
   return (
     <li className="border-2 border-gray-700 rounded-lg mt-4 p-5">
       <div className="rounded-md bg-gray-500 max-w-max">
-        // @@TODO: Add Rendered componentss
+        {/* // @@TODO: Add Rendered componentss */}
       </div>
     </li>
   );
 }
 
-
 export async function getStaticProps() {
-  const featuredAngkaKejadian = require('../data/angka-kejadian.json');
+  const data = require('data/scraper-covidsda-sheets.json');
   return {
     props: {
-      featuredAngkaKejadian,
+      data,
     },
   };
 }
