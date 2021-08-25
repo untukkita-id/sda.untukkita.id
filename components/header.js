3298707523d1e31a09dc7d8279cde6a5f93da5ca
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-26 00:46:46 +0700

    chore(seo): replace seo image meta

--- components/header.js
+++ components/header.js
@@ -1,12 +1,14 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import NavbarButton from 'components/navbar-bottom';
 
 export default function Header({ title }) {
   return (
     <>
-      <header id="header" className="bg-gray-200 py-4 px-8 flex gap-4">
-        <Image src="/logo.svg" height={30} width={50} alt="UntukKita.SDA Logo" />
-        <p className="text-lg">{title}</p>
+      <header id="header" className="bg-gray-200 py-4 px-8 flex gap-4 items-center">
+        <Link href="/" passHref>
+          <img src="/logo-transparent.svg" height={50} width={50} alt="UntukKita.SDA Logo" />
+        </Link>
+        <p className="text-xl">{title}</p>
       </header>
       <NavbarButton />
     </>
