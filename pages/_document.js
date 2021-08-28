4919229a416beb4c5e83cef5aeb8d19061d94cc3
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-29 03:17:41 +0700

    chore: root manifest json dir

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
           <meta property="twitter:image" content="/opengraph.jpg" />
 
           {/* Manifest Meta Tag */}
-          <link rel="manifest" href="manifest.json" />
+          <link rel="manifest" href="/manifest.json" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="application-name" content="Laman Informasi Vaksinasi" />
