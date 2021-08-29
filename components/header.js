0a8f943b879ee44c3f804747f27ebc32aad7b2cb
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-29 13:52:40 +0700

    refactor: use next/image instead img tag

--- components/header.js
+++ components/header.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import NavbarButton from 'components/navbar-bottom';
+import Image from 'next/image';
 
 export default function Header({ title }) {
   return (
     <>
       <header id="header" className="bg-gray-200 py-4 px-8 flex gap-4 items-center">
         <Link href="/" passHref>
-          <img src="/logo-transparent.svg" height={50} width={50} alt="UntukKita.SDA Logo" />
+          <Image src="/logo-transparent.svg" height={50} width={50} alt="UntukKita.SDA Logo" />
         </Link>
         <p className="text-xl">{title}</p>
       </header>
