c280145880297dfa81efba4874114206d68f8d2c
Wahyu Dwi Pamungkas <54252239+Wahyu-Dwi@users.noreply.github.com>
2021-08-29 22:24:00 +0700

    fix: navbar faskes

--- components/navbar-faskes.js
+++ components/navbar-faskes.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NavbarFaskes() {
   return (
-    <nav className="static top-5 bg-gray-300 w-screen h-12 px-4 flex flex-row gap-2 justify-center">
+    <nav className="static top-5 bg-gray-300 h-12 px-4 flex flex-row gap-2 justify-center">
       <Link href="/fasilitas-kesehatan" passHref>
         <div className="flex flex-col flex-grow items-center justify-center active:bg-gray-200 hover:bg-gray-100 font-semibold text-gray-800">
           <p>Rumah Sakit</p>
